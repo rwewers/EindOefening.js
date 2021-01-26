6885ce53455676061f5fd3e6d9571eb9c1a6c853
rwewers <remco@wewers.nl>
2021-01-26 14:06:25 +0100

    demodrop validation toegevoegd en state

---
+++ src/components/InputFieldValidation.js
@@ -0,0 +1,105 @@
+import React, {forwardRef, useImperativeHandle} from "react";
+import './InputFieldValidation.css';
+
+const InputField = forwardRef((props, ref) => {
+    const[value, setValue] = React.useState("");
+    const [error, setError] = React.useState("");
+    const handleChange = (event) => {
+
+        setValue(event.target.value)
+        setError("");
+        props.onChange(event.target.name, event.target.value)
+    }
+
+    const validate = () => {
+        // return true if is valid
+        // else return false
+
+        if(props.validation) {
+        const rules = props.validation.split("|");
+            for (let i = 0; i < rules.length ; i++) {
+                const current = rules[i];
+
+                if(current === "required"){
+                    if(!value){
+                        setError("This field is required")
+                        return false;
+                    }
+                }
+
+                if(current === "email"){
+
+                    const pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+
+                    if(!pattern.test(value)){
+                        setError("invalid email");
+                    }
+
+                }
+
+                const pair = current.split(":");
+                switch (pair[0]){
+                    case "min":
+                        if(value.length < pair[1]){
+                            setError(`This field must be at least ${pair[1]} characters long`)
+                            return false;
+                        }
+                        break;
+
+                        case "max":
+                        if(value.length > pair[1]){
+                            setError(`This field can be longer than ${pair[1]} characters long`)
+                            return false;
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+
+
+            }
+        }
+
+        return true;
+    }
+
+    useImperativeHandle(ref,()=>{
+        return {
+            validate: () => validate ()
+        }
+    })
+
+    return(
+        <div>
+            {props.label && (
+                <label>{props.label}</label>
+            )}
+            <input
+                placeholder={props.placeholder}
+                name={props.name}
+                onChange={(event) => handleChange(event)}
+                id={props.id}
+                type={props.type}
+                value={props.value ? props.value : value}
+
+            />
+            {error && (
+                <p className="error">{error} </p>
+            )}
+        </div>
+    )
+
+})
+InputField.defaultProp={
+    placeholder: "",
+    name: "",
+    type: "text",
+    value: "",
+    id: "",
+    validation: ""
+
+
+}
+
+export default InputField;
--- src/pages/demodrop/Demodrop.js
+++ src/pages/demodrop/Demodrop.js
@@ -1,48 +1,109 @@
 import React from 'react';
 import './Demodrop.css';
+import InputField from "../../components/InputFieldValidation";
 
 
 function Demodrop() {
 
+
+    const inputRefs = React.useRef([
+        React.createRef(), React.createRef(), React.createRef(), React.createRef(), React.createRef(), React.createRef(), React.createRef(), React.createRef()
+    ]);
+    const[data, setData] = React.useState({});
+
+    const handleChange = (name, value) => {
+    setData(prev => ({ ...prev, [name]: value }))
+    }
+
+    console.log(data);
+    console.log(inputRefs);
+
+    const submitForm = (e) => {
+        e.preventDefault();
+
+        let isValid = true;
+
+        for(let i = 0; i<inputRefs.current.length; i++ ){
+            const valid =  inputRefs.current[i].current.validate()
+            console.log(valid);
+            if(!valid){
+                isValid= false
+            }
+        }
+        if(!isValid){
+            return;
+        }
+
+        //carry on as normal
+    }
+
     return (
-            <div>
-             <h1 id="demodrop">DEMODROP</h1>
-                <form className="form">
-                    <label id="text">
-                        NAME *
-                        <input type="text" name="name" />
-                    </label>
-                    <label id="text">
-                        EMAIL ADDRESS *
-                        <input type="text" name="name" />
-                    </label>
-                    <label id="text">
-                        COUNTRY *
-                        <input type="text" name="name" />
-                    </label>
-                    <label id="text">
-                        FACEBOOK
-                        <input type="text" name="name" />
-                    </label>
-                    <label id="text">
-                        INSTAGRAM
-                        <input type="text" name="name" />
-                    </label>
-                    <label id="text">
-                        ARTISTNAME - SONG NAME *
-                        <input type="text" name="name" />
-                    </label>
-                    <label id="text">
-                        MUSIC URL *
-                        <input type="text" name="name" />
-                    </label>
-                    <label id="text">
-                        PERSONAL MESSAGE
-                        <input id="personalText" type="text" name="name" />
-                    </label>
+                    <div>
+                    <h1 id="demodrop">DEMODROP</h1>
+                   <form onSubmit={submitForm}>
+                    <InputField
+                        ref={inputRefs.current[0]}
+                        name="name"
+                        label="Name *"
+                        onChange ={handleChange}
+                        validation={"required|min:5|max:12"}
+                    />
+                       <InputField
+                           ref={inputRefs.current[1]}
+                           name="emailadress"
+                           label="Email adress * "
+                           onChange ={handleChange}
+                           validation={"required|email"}
+                       />
+                       <InputField
+                           ref={inputRefs.current[2]}
+                           name="country"
+                           label="Country * "
+                           onChange ={handleChange}
+                           validation={"required"}
+                       />
+                       <InputField
+                           ref={inputRefs.current[3]}
+                           name="facebook"
+                           label="Facebook"
+                           onChange ={handleChange}
+                       />
+                       <InputField
+                           ref={inputRefs.current[4]}
+                           name="instagram"
+                           label="Instagram"
+                           onChange ={handleChange}
+                       />
+                       <InputField
+                           ref={inputRefs.current[5]}
+                           name="atristname-songname"
+                           label="Artistname - Songname *"
+                           onChange ={handleChange}
+                           validation={"required"}
+                       />
+                       <InputField
+                           ref={inputRefs.current[6]}
+                           name="music-url"
+                           label="Music-url *"
+                           onChange ={handleChange}
+                           validation={"required"}
+                       />
+                       <InputField
+                           ref={inputRefs.current[7]}
+                           id="personalText"
+                           name="personal-messages"
+                           label="Personal messages *"
+                           onChange ={handleChange}
+                           validation={"required|max:250"}
+                       />
+
+                       <button type="submit" >
+                        Submit
+                    </button>
 
                     </form>
-            </div>
+                    </div>
+
     );
 }
 export default Demodrop;
