d56b3da4d30b29c16c9ba86a0b3f34881174d2af
rwewers <remco@wewers.nl>
2021-02-05 15:33:14 +0100

    different backgrounds

--- src/pages/Customer/homepage/Homepage.js
+++ src/pages/Customer/homepage/Homepage.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
-import background from "../../../782147.jpg";
+import background1 from "../../../782147.jpg";
+import DonDiablo_into_the_unknown from "../../../assets/DonDiablo_into_the_unkown.PNG"
+import background2 from "../../../assets/138618.jpg";
+
+import background3 from "../../../assets/138616.jpg";
+
 import './Homepage.css';
 import {
     Link,
@@ -19,9 +24,10 @@ function Homepage(){
     return(
     <html>
     <TopMenu />
-    <body>
+    <body className="parent">
       <main>
-        <img src={background} className="background-image" alt="logo"/>
+        <img src={background3} className="background-image3" alt="logo"/>
+          <img src={DonDiablo_into_the_unknown} className="don_diablo_unknown" alt="logo"/>
     </main>
 
 
